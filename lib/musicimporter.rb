a832b8e7d5caa6aea2a00e8827c983e0b890fcfd
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-05-20 03:09:24 +0000

    automatically backed up by learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,13 +1,15 @@
 class MusicImporter
  attr_accessor :path
 
+ def initialize(path)
+   @path = path
+end
+
 
   def self.import(list_of_filenames)
     list_of_filenames.each{ |filename| Song.create_from_filename(filename) }
   end
 
 
-  def initialize(path)
-    @path = path
-end
+
 end
