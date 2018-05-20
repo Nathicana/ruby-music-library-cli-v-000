0be372116d4b2d804ce7b2526c8c54fe1090cdaa
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-05-20 01:59:15 +0000

    automatically backed up by learn

--- lib/artists.rb
+++ lib/artists.rb
@@ -28,8 +28,7 @@ end
 
 def add_song(song)
   @songs << song
-  if song.artist != self
-    song.artist = self
+ song.artist << self unless song.artist.include?(self)
     # tells new song that it belongs to an artist
 end
 end
