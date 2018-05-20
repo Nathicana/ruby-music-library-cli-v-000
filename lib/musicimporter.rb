f348a13c8d4461d01ccb68797063d09adc3cd2c5
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-05-20 03:26:55 +0000

    automatically backed up by learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -6,8 +6,8 @@ class MusicImporter
 end
 
 
-  def self.import(list_of_filenames)
-    list_of_filenames.each{ |filename| Song.create_from_filename(filename) }
+  def import
+    files.each{ |filename| Song.create_from_filename(filename) }
   end
 
 def files
